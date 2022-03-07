4da67a579bef010c36410c329b1bcd61e8dbb232
Amit Shinde <AmitS13@citiustech.com>
2022-03-07 11:30:18 +0530

    commit

---
+++ API/Controllers/ProducerController.cs
@@ -0,0 +1,36 @@
+﻿using API.Models;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private ProducerConfig _config;
+
+        public ProducerController(ProducerConfig config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("Send")]
+        public async Task<IActionResult> Get(string topic, [FromBody] Employee emp)
+        {
+            string SerializedEmployee = JsonConvert.SerializeObject(emp);
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value=SerializedEmployee });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok(true);
+            }
+        }
+    }
+}
